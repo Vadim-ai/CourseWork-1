c38e3715282b8fe7901090b6bfa78d264c8e7898
SkyPro <vadim.phobia@gmail.com>
2022-05-19 00:29:21 +0300

    Коммит 2

---
+++ src/Employee.java
@@ -0,0 +1,49 @@
+public class Employee {
+
+    private String fullname;
+    private double salary;
+    private int department;
+
+    private int id;
+
+    private static int Count;
+
+
+    public Employee(String fullname, double salary, int department) {
+    this.fullname = fullname;
+    this.salary = salary;
+    this.department = department;
+    this.id = Count;
+    Count++;
+}
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return id + " " + fullname + " " +  salary + " " + department;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
 
+        Employee[] employees = new Employee[10];
+
+        Employee employee0 = new Employee("Суслова Капитолина Вадимовна", 250_000.0, 1);
+        Employee employee1 = new Employee("Суслова Валерия Вадимовна", 200_000.0, 2);
+
+        AddEmployee(employee0, employees);
+        AddEmployee(employee1, employees);
+        PrintAllEmployees(employees);
+
+    }
+
+    public static void AddEmployee(Employee employee, Employee[] employees) {
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null) {
+                employees[i] = employee;
+                break;
+            }
+        }
+    }
+
+    public static void PrintAllEmployees(Employee[] employees) {
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null) {
+                System.out.println(employees[i].toString());
+            }
+        }
+    }
+
+    public static double CalculateAllSalary(Employee[] employees) {
+
     }
 }
